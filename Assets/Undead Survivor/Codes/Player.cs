65d3eccafe6c971427a4a9a447fe59c60d7dc9d3
Zzanzzan <cykim1228@gmail.com>
2025-06-27 00:55:29 +0900

    ✨ 플레이어 셀 애니메이션 구현
    - 유니티 뱀서라이크 03
    - Animator 이용하여 Stand , Run , Dead 구현
    - Animator Override Controller 로 기존의 Player 애니메이션 재활용

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -7,22 +8,26 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    void Update()
-    {
-        // inputVec.x = Input.GetAxis("Horizontal");
-        // inputVec.y = Input.GetAxis("Vertical");
+    // void Update()
+    // {
+    //     // inputVec.x = Input.GetAxis("Horizontal");
+    //     // inputVec.y = Input.GetAxis("Vertical");
 
-        // 부드럽게 이동 (GetAxis 는 보정이 들어감)
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
+    //     // 부드럽게 이동 (GetAxis 는 보정이 들어감)
+    //     inputVec.x = Input.GetAxisRaw("Horizontal");
+    //     inputVec.y = Input.GetAxisRaw("Vertical");
 
-    }
+    // }
 
     void FixedUpdate()
     {
@@ -32,12 +37,29 @@ public class Player : MonoBehaviour
         // // 2. 속도 제어
         // rigid.linearVelocity = inputVec;
 
-        // normalized 는 대각선 이동 시 피타고라스 정의 에 의해 루트 2 가 되도록 해줌
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        // // normalized 는 대각선 이동 시 피타고라스 정의 에 의해 루트 2 가 되도록 해줌
+        // Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
 
         // 3. 위치 이동
         rigid.MovePosition(rigid.position + nextVec);
 
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
     
 }
